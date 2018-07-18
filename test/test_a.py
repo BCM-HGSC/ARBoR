79303d5345ea0f854c7689f9367921baa30e2969
Walker Hale IV <walker.hale.iv@gmail.com>
2018-07-18 16:46:26 -0500

    Add test_read_ledger

--- test/test_a.py
+++ test/test_a.py
@@ -1,5 +1,6 @@
 import pytest
 
+import ledger_validator
 from ledger_validator import run
 
 
@@ -41,3 +42,32 @@ def test_ledger_bad_key():
             True,
             'test/resources/eValidate_ledger.json',
             'test/resources/bad-key.key')
+
+
+def test_read_ledger():
+    ledger_validator.RECORDS = None
+    ledger_validator.RECORDS_BY_HASH = {}
+    ledger_validator.read_ledger('test/resources/eValidate_ledger_00.json')
+    EXEPECTED_RECORDS = [
+        {
+            u'previousblockhash': u'',
+            u'localid': u'smpid_0',
+            u'filehash': u'n0Od+EuQKQX9pp2w4jCOhyUixW9z+NLDVa14220cPTkYkojRc82'
+                         u'tgcRTOfF+P9qbQpnLRIMjTNlNBKD+A5XGtQ==',
+            u'blockindex': 0,
+            u'filesignature': u'CVzfsk4X6Uy8uRiOwcgHWE1ouaa7+1XxLb3s3xWR2MCEMj'
+                              u'3h/uuIs8ueADBSZ+gPheEFHuV9ESvikMI96TWR80MiotYc'
+                              u'S2AYOpJO7x2VWT0SAa95YEPimShLgPyRXGf44JZPXha/s'
+                              u'/0ySI5LePqHuz54l+0ThYHDkQz87irEc1Y=',
+            u'blocktimestamp': 1530889311.415133,
+            u'patientid': u'22651339',
+            u'rptid': u'rptid_100002',
+            u'rpttype': u'xml',
+            u'rptdate': 1064739376262
+        }
+    ]
+    assert ledger_validator.RECORDS == EXEPECTED_RECORDS
+    block = EXEPECTED_RECORDS[0]
+    assert ledger_validator.RECORDS_BY_HASH == {
+        block[u'filehash']: block
+    }
