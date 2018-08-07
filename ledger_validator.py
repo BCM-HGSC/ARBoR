c7dd902f36490c60c077f53756c36f62c94a5774
Walker Hale IV <walker.hale.iv@gmail.com>
2018-08-07 14:46:03 -0500

    Fix bug #33

--- ledger_validator.py
+++ ledger_validator.py
@@ -228,10 +228,11 @@ def verify_block(verifier, filehash):
     Returns True if block can be verified by its digital signature.'''
     block = get_record_by_hash(filehash)
     if block:
-        blocksig = b64decode(block[BLOCKSIG])
+        copy = dict(block)
+        blocksig = b64decode(copy[BLOCKSIG])
         # Remove sig from block before hashing the block to .
-        del block[BLOCKSIG]
-        blockhash = hash_block(block)
+        del copy[BLOCKSIG]
+        blockhash = hash_block(copy)
         return verifier.verify(blockhash, blocksig)
     else:
         return False
